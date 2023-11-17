86a01b1fd3c547897a25711bd9943f7a2c0e604d
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2023-11-18 00:23:13 +0600

    added

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const cors = require('cors');
+
+const categories = require('./Data/Catagories.json');
+
+const rooms = require('./Data/Room.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello!')
+  res.send('Whispering Willow Inn Server is running ')
+})
+app.get('/categories', (req, res) => {
+  res.send(categories);
+})
+
+/**
+ * all rooms loaded*/
+
+app.get('/rooms', (req, res) => {
+  res.send(rooms);
+})
+
+/**
+ * individual rooms we can get
+ * */
+app.get('/rooms/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectNews = rooms.find(n => n._id === id);
+  res.send(selectNews);
+})
+
+/**
+ * specific rooms
+ * */
+
+app.get('/categories/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  console.log(id)
+  if (id === 0) {
+    res.send(rooms)
+  }
+  else {
+    const categoryNews = rooms.filter(n => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+  }
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Narrative News Network is running ${port}`)
 })
+
+
+
